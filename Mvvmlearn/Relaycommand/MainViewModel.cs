3424323e60561e6ed84b1669b94b2cfe0d053aa9
Lin Jie <ljlovemichael@hotmail.com>
2016-03-02 17:57:24 +0800

    添加类

---
+++ Mvvmlearn/Relaycommand/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Relaycommand
+{
+    public class MainViewModel
+    {
+        public ICommand TestCommand { get; set; }
+        public MainViewModel()
+        {
+           // TestCommand = new RelayCommand();
+        }
+    }
+}
