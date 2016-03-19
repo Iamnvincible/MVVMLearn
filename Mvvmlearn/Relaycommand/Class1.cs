b0714fc53b8d167785d6052c3c082a6c5bd9ce4c
Lin Jie <ljlovemichael@hotmail.com>
2016-03-19 14:29:40 +0800

    手写一遍MVVMLight

---
+++ Mvvmlearn/Relaycommand/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Relaycommand
+{
+    class Class1
+    {
+
+
+        //public int MyProperty
+        //{
+        //    get { return (int)GetValue(MyPropertyProperty); }
+        //    set { SetValue(MyPropertyProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty MyPropertyProperty =
+        //    DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+        //public static int GetMyProperty(DependencyObject obj)
+        //{
+        //    return (int)obj.GetValue(MyPropertyProperty);
+        //}
+
+        //public static void SetMyProperty(DependencyObject obj, int value)
+        //{
+        //    obj.SetValue(MyPropertyProperty, value);
+        //}
+
+        //// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty MyPropertyProperty =
+        //    DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+    }
+}
