576a639b1e779847f2cef048351802702ddc5c76
amvincible <amvincible@SURFACE>
2017-02-09 23:05:28 +0800

    新年再学MVVMLight

---
+++ Mvvmlearn/MVVM2017/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace MVVM2017.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool _isactive
+            ;
+
+        public bool IsActive
+        {
+            get { return _isactive; }
+            set { Set(ref _isactive, value); }
+        }
+        public ICommand ChangeProgressCommand { get; set; }
+        private void ChangeProgress()
+        {
+            IsActive = !IsActive;
+        }
+        public MainViewModel()
+        {
+            IsActive = true;
+            ChangeProgressCommand = new RelayCommand(ChangeProgress);
+        }
+    }
+}
---
+++ Mvvmlearn/MVVM2017/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM2017.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // Create design time view services and models
+               // SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            }
+            else
+            {
+                // Create run time view services and models
+               // SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainPageInstance
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
